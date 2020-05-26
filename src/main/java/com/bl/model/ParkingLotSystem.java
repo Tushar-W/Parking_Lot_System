07b04503702c5cfdb812f8f6748b515e416dcd45
Tushar-W <twankhede95@gmail.com>
2020-05-26 11:02:42 -0700

    ADDED:code to create functionality for check vehicle park in parking lot

---
+++ src/main/java/com/bl/model/ParkingLotSystem.java
@@ -0,0 +1,17 @@
+/**
+ * @Purpose: developing parking lot system
+ * @author: Tushar W
+ * @date: 25-05-20
+ */
+package com.bl.model;
+
+public class ParkingLotSystem {
+    /**
+     * method to check vehicle park or not in parking lot
+     * @param vehicle
+     * @return true or false
+     */
+    public boolean park(Object vehicle) {
+        return true;
+    }
+}
