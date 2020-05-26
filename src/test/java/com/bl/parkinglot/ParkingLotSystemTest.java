f80277a7c1a5eb4feec6074579c95a644fbdd57a
Tushar-W <twankhede95@gmail.com>
2020-05-26 11:03:24 -0700

    ADDED:tested functionality for vehicle park in parking lot or not

---
+++ src/test/java/com/bl/parkinglot/ParkingLotSystemTest.java
@@ -0,0 +1,15 @@
+package com.bl.parkinglot;
+
+import com.bl.model.ParkingLotSystem;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ParkingLotSystemTest {
+
+    @Test
+    public void givenAVehicle_WhenParked_ShouldReturnTrue() {
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+        boolean isParked = parkingLotSystem.park(new Object());
+        Assert.assertTrue(isParked);
+    }
+}
