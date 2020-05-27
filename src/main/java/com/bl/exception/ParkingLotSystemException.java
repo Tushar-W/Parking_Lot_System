301b36e8cae500ebafe812fb1fd4850982867d1b
Tushar-W <twankhede95@gmail.com>
2020-05-27 02:50:07 -0700

    ADDED:code to create functionality check given vehicle already parked in parking lot

---
+++ src/main/java/com/bl/exception/ParkingLotSystemException.java
@@ -0,0 +1,23 @@
+package com.bl.exception;
+
+public class ParkingLotSystemException extends Throwable {
+    /**
+     * enum for take constants Exception Types
+     */
+    public enum ExceptionType{
+        PARKING_LOTS_IS_EMPTY, PARKING_LOT_FULL;
+    }
+    /**
+     * object of enum class ExceptionType
+     */
+    public ExceptionType type;
+    /**
+     * constructor to take two input values
+     * @param type
+     * @param message
+     */
+    public ParkingLotSystemException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+}
--- src/main/java/com/bl/model/ParkingLotSystem.java
+++ src/main/java/com/bl/model/ParkingLotSystem.java
@@ -5,13 +5,45 @@
  */
 package com.bl.model;
 
+import com.bl.exception.ParkingLotSystemException;
+
 public class ParkingLotSystem {
     /**
-     * method to check vehicle park or not in parking lot
+     * field vehicle use to store vehicle
+     */
+    private Object vehicle;
+    /**
+     * method to check vehicle park or not to parking lot
      * @param vehicle
      * @return true or false
      */
-    public boolean park(Object vehicle) {
-        return true;
+    public void park(Object vehicle) throws ParkingLotSystemException {
+        if (this.vehicle == null) {
+            this.vehicle = vehicle;
+        }
+        throw new ParkingLotSystemException("PARKING_LOT_IS_FULL", ParkingLotSystemException.ExceptionType.PARKING_LOT_FULL);
+    }
+    /**
+     * method to check given vehicle unPark or not from parking lot
+     * @param vehicle
+     * @return true or false
+     */
+    public void unPark(Object vehicle) throws ParkingLotSystemException {
+        if (this.vehicle == null)
+            throw new ParkingLotSystemException("PARKING_LOT_IS_EMPTY", ParkingLotSystemException.ExceptionType.PARKING_LOTS_IS_EMPTY);
+        if (this.vehicle.equals(vehicle))
+            this.vehicle = null;
+    }
+
+    public boolean isVehicleParked(Object vehicle) {
+        if (this.vehicle.equals(vehicle))
+            return true;
+        return false;
+    }
+
+    public boolean isVehicleUnParked(Object vehicle) {
+        if (this.vehicle.equals(vehicle))
+            return true;
+        return false;
     }
 }
