e35464a84f2445254882d8ebd7ba43d3005a4be5
Tushar-W <twankhede95@gmail.com>
2020-05-27 02:48:59 -0700

    ADDED:tested functionality when vehicle already parked in parking lot then throw exception

--- src/test/java/com/bl/parkinglot/ParkingLotSystemTest.java
+++ src/test/java/com/bl/parkinglot/ParkingLotSystemTest.java
@@ -1,15 +1,41 @@
 package com.bl.parkinglot;
 
+import com.bl.exception.ParkingLotSystemException;
 import com.bl.model.ParkingLotSystem;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotSystemTest {
+    ParkingLotSystem parkingLotSystem = null;
+    Object vehicle = null;
+
+    @Before
+    public void setUp() throws Exception {
+        vehicle = new Object();
+        parkingLotSystem = new ParkingLotSystem();
+    }
 
     @Test
     public void givenAVehicle_WhenParked_ShouldReturnTrue() {
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
-        boolean isParked = parkingLotSystem.park(new Object());
-        Assert.assertTrue(isParked);
+        try {
+            parkingLotSystem.park(vehicle);
+            boolean isParked = parkingLotSystem.isVehicleParked(vehicle);
+            Assert.assertTrue(isParked);
+        } catch (ParkingLotSystemException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void givenAVehicle_WhenAlreadyParked_ShouldReturnThrowException() {
+        try {
+            parkingLotSystem.park(vehicle);
+            parkingLotSystem.park(new Object());
+        } catch (ParkingLotSystemException e) {
+            Assert.assertEquals(ParkingLotSystemException.ExceptionType.PARKING_LOT_FULL,e.type);
+        }
+
     }
 }
